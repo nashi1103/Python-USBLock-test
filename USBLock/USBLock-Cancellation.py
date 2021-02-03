aa2f77ddff23b6fc66d3ab608cfb1ebbb56929f0
nkaii <71241556+nashi1103@users.noreply.github.com>
2021-02-03 22:39:16 +0900

    Add files via upload

---
+++ USBLock/USBLock-Cancellation.py
@@ -0,0 +1,15 @@
+#Python 3.9
+import winreg
+
+RegistryKeyName = r"SYSTEM\\CurrentControlSet\\Services\\USBSTOR"
+
+try:
+#キーに繋ぐ
+    RegConnection = winreg.ConnectRegistry(None,winreg.HKEY_LOCAL_MACHINE)
+#キーを開く
+    Key = winreg.OpenKeyEx(RegConnection,RegistryKeyName,reserved=0,access=winreg.KEY_WRITE)
+#レジストリの書き換え
+    settings = winreg.SetValueEx(Key,"Start",0,winreg.REG_DWORD,3)
+    winreg.CloseKey(Key)
+except:
+    print("正常に動作しませんでした。")
---
+++ USBLock/USBLock.bat
@@ -0,0 +1,3 @@
+C:\Users\seiya\AppData\Local\Programs\Python\Python39\Python.exe C:\Users\seiya\Desktop\VScode\USBLock\USBLock.py
+
+pause
